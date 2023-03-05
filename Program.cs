512de767f801baeffe4fcaa4bfb493b91aabd9d6
waadsulaiman <waad.sulaiman@outlook.com>
2023-03-05 19:38:11 +0100

    The bot can now run

--- Program.cs
+++ Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Discord;
+using Discord.Addons.Hosting;
+using Discord.WebSocket;
+using GodBot;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+DotNetEnv.Env.Load();
+var host = Host.CreateDefaultBuilder()
+    .ConfigureDiscordHost((_, config) =>
+    {
+        config.SocketConfig = new DiscordSocketConfig
+        {
+            LogLevel = LogSeverity.Verbose,
+            GatewayIntents = GatewayIntents.All,
+            AlwaysDownloadUsers = true,
+            MessageCacheSize = 1000,
+            LogGatewayIntentWarnings = false,
+        };
+
+        config.Token = Environment.GetEnvironmentVariable("DISCORD_TOKEN") ?? string.Empty;
+    })
+    .UseInteractionService((context, config) =>
+    {
+        config.LogLevel = LogSeverity.Info;
+        config.UseCompiledLambda = true;
+    })
+    .ConfigureServices((context, services) =>
+    {
+        //Add any other services here
+        services.AddHostedService<InteractionHandler>();
+    }).Build();
+
+await host.RunAsync();
